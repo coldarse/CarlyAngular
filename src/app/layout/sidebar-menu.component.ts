73dd7617fd773226db9fe1f5d4889f304bde7e8e
coldarse <37390180+coldarse@users.noreply.github.com>
2021-07-13 08:31:54 +0800

    Minor Changes to UI and Added Functionality
    - Adjusted Theme to better follow Carly website.
    - Added both Desc & Price to adding/editing principal

--- src/app/layout/sidebar-menu.component.ts
+++ src/app/layout/sidebar-menu.component.ts
@@ -44,7 +44,11 @@ export class SidebarMenuComponent extends AppComponentBase implements OnInit {
     getMenuItems(): MenuItem[] {
         return [
             // new MenuItem(this.l('About'), '/app/about', 'fas fa-info-circle'),
-            // new MenuItem(this.l('HomePage'), '/app/home', 'fas fa-home'),
+            new MenuItem(
+                this.l('HomePage'), 
+                '/app/home', 
+                'fas fa-home'
+            ),
             new MenuItem(
                 this.l('Roles'),
                 '/app/roles',
@@ -84,60 +88,9 @@ export class SidebarMenuComponent extends AppComponentBase implements OnInit {
             new MenuItem(
                 this.l('GeneratedVouchers'),
                 '/app/generatedVouchers',
-                'fas fa-box-open',
+                'fas fa-cogs',
                 'Pages.GeneratedVouchers'
             ),
-            // new MenuItem(this.l('MultiLevelMenu'), '', 'fas fa-circle', '', [
-            //     new MenuItem('ASP.NET Boilerplate', '', 'fas fa-dot-circle', '', [
-            //         new MenuItem(
-            //             'Home',
-            //             'https://aspnetboilerplate.com?ref=abptmpl',
-            //             'far fa-circle'
-            //         ),
-            //         new MenuItem(
-            //             'Templates',
-            //             'https://aspnetboilerplate.com/Templates?ref=abptmpl',
-            //             'far fa-circle'
-            //         ),
-            //         new MenuItem(
-            //             'Samples',
-            //             'https://aspnetboilerplate.com/Samples?ref=abptmpl',
-            //             'far fa-circle'
-            //         ),
-            //         new MenuItem(
-            //             'Documents',
-            //             'https://aspnetboilerplate.com/Pages/Documents?ref=abptmpl',
-            //             'far fa-circle'
-            //         ),
-            //     ]),
-            //     new MenuItem('ASP.NET Zero', '', 'fas fa-dot-circle', '', [
-            //         new MenuItem(
-            //             'Home',
-            //             'https://aspnetzero.com?ref=abptmpl',
-            //             'far fa-circle'
-            //         ),
-            //         new MenuItem(
-            //             'Features',
-            //             'https://aspnetzero.com/Features?ref=abptmpl',
-            //             'far fa-circle'
-            //         ),
-            //         new MenuItem(
-            //             'Pricing',
-            //             'https://aspnetzero.com/Pricing?ref=abptmpl#pricing',
-            //             'far fa-circle'
-            //         ),
-            //         new MenuItem(
-            //             'Faq',
-            //             'https://aspnetzero.com/Faq?ref=abptmpl',
-            //             'far fa-circle'
-            //         ),
-            //         new MenuItem(
-            //             'Documents',
-            //             'https://aspnetzero.com/Documents?ref=abptmpl',
-            //             'far fa-circle'
-            //         )
-            //     ])
-            // ])
         ];
     }
 
